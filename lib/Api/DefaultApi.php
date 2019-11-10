104f9acd1a56e89fceef31a60e573c3294234d94
Konstantin Kabanov <fever9@gmail.com>
2019-11-10 11:57:36 +0700

    add devicesNearestGet

--- lib/Api/DefaultApi.php
+++ lib/Api/DefaultApi.php
@@ -5084,6 +5084,298 @@ class DefaultApi
     }
 
     /**
+     * Operation devicesNearestGet
+     *
+     * Searches for nearest Device
+     *
+     * @param  string $tractoken tractoken (required)
+     * @param  float $latitude latitude (required)
+     * @param  float $longitude longitude (required)
+     *
+     * @throws \Swagger\Client\ApiException on non-2xx response
+     * @throws \InvalidArgumentException
+     * @return \Swagger\Client\Model\Device
+     */
+    public function devicesNearestGet($tractoken, $latitude, $longitude)
+    {
+        list($response) = $this->devicesNearestGetWithHttpInfo($tractoken, $latitude, $longitude);
+        return $response;
+    }
+
+    /**
+     * Operation devicesNearestGetWithHttpInfo
+     *
+     * Searches for nearest Device
+     *
+     * @param  string $tractoken (required)
+     * @param  float $latitude (required)
+     * @param  float $longitude (required)
+     *
+     * @throws \Swagger\Client\ApiException on non-2xx response
+     * @throws \InvalidArgumentException
+     * @return array of \Swagger\Client\Model\Device, HTTP status code, HTTP response headers (array of strings)
+     */
+    public function devicesNearestGetWithHttpInfo($tractoken, $latitude, $longitude)
+    {
+        $returnType = '\Swagger\Client\Model\Device';
+        $request = $this->devicesNearestGetRequest($tractoken, $latitude, $longitude);
+
+        try {
+            $options = $this->createHttpClientOption();
+            try {
+                $response = $this->client->send($request, $options);
+            } catch (RequestException $e) {
+                throw new ApiException(
+                    "[{$e->getCode()}] {$e->getMessage()}",
+                    $e->getCode(),
+                    $e->getResponse() ? $e->getResponse()->getHeaders() : null,
+                    $e->getResponse() ? $e->getResponse()->getBody()->getContents() : null
+                );
+            }
+
+            $statusCode = $response->getStatusCode();
+
+            if ($statusCode < 200 || $statusCode > 299) {
+                throw new ApiException(
+                    sprintf(
+                        '[%d] Error connecting to the API (%s)',
+                        $statusCode,
+                        $request->getUri()
+                    ),
+                    $statusCode,
+                    $response->getHeaders(),
+                    $response->getBody()
+                );
+            }
+
+            $responseBody = $response->getBody();
+            if ($returnType === '\SplFileObject') {
+                $content = $responseBody; //stream goes to serializer
+            } else {
+                $content = $responseBody->getContents();
+                if ($returnType !== 'string') {
+                    $content = json_decode($content);
+                }
+            }
+
+            return [
+                ObjectSerializer::deserialize($content, $returnType, []),
+                $response->getStatusCode(),
+                $response->getHeaders()
+            ];
+
+        } catch (ApiException $e) {
+            switch ($e->getCode()) {
+                case 200:
+                    $data = ObjectSerializer::deserialize(
+                        $e->getResponseBody(),
+                        '\Swagger\Client\Model\Device',
+                        $e->getResponseHeaders()
+                    );
+                    $e->setResponseObject($data);
+                    break;
+            }
+            throw $e;
+        }
+    }
+
+    /**
+     * Operation devicesNearestGetAsync
+     *
+     * Searches for nearest Device
+     *
+     * @param  string $tractoken (required)
+     * @param  float $latitude (required)
+     * @param  float $longitude (required)
+     *
+     * @throws \InvalidArgumentException
+     * @return \GuzzleHttp\Promise\PromiseInterface
+     */
+    public function devicesNearestGetAsync($tractoken, $latitude, $longitude)
+    {
+        return $this->devicesNearestGetAsyncWithHttpInfo($tractoken, $latitude, $longitude)
+            ->then(
+                function ($response) {
+                    return $response[0];
+                }
+            );
+    }
+
+    /**
+     * Operation devicesNearestGetAsyncWithHttpInfo
+     *
+     * Searches for nearest Device
+     *
+     * @param  string $tractoken (required)
+     * @param  float $latitude (required)
+     * @param  float $longitude (required)
+     *
+     * @throws \InvalidArgumentException
+     * @return \GuzzleHttp\Promise\PromiseInterface
+     */
+    public function devicesNearestGetAsyncWithHttpInfo($tractoken, $latitude, $longitude)
+    {
+        $returnType = '\Swagger\Client\Model\Device';
+        $request = $this->devicesNearestGetRequest($tractoken, $latitude, $longitude);
+
+        return $this->client
+            ->sendAsync($request, $this->createHttpClientOption())
+            ->then(
+                function ($response) use ($returnType) {
+                    $responseBody = $response->getBody();
+                    if ($returnType === '\SplFileObject') {
+                        $content = $responseBody; //stream goes to serializer
+                    } else {
+                        $content = $responseBody->getContents();
+                        if ($returnType !== 'string') {
+                            $content = json_decode($content);
+                        }
+                    }
+
+                    return [
+                        ObjectSerializer::deserialize($content, $returnType, []),
+                        $response->getStatusCode(),
+                        $response->getHeaders()
+                    ];
+                },
+                function ($exception) {
+                    $response = $exception->getResponse();
+                    $statusCode = $response->getStatusCode();
+                    throw new ApiException(
+                        sprintf(
+                            '[%d] Error connecting to the API (%s)',
+                            $statusCode,
+                            $exception->getRequest()->getUri()
+                        ),
+                        $statusCode,
+                        $response->getHeaders(),
+                        $response->getBody()
+                    );
+                }
+            );
+    }
+
+    /**
+     * Create request for operation 'devicesNearestGet'
+     *
+     * @param  string $tractoken (required)
+     * @param  float $latitude (required)
+     * @param  float $longitude (required)
+     *
+     * @throws \InvalidArgumentException
+     * @return \GuzzleHttp\Psr7\Request
+     */
+    protected function devicesNearestGetRequest($tractoken, $latitude, $longitude)
+    {
+        // verify the required parameter 'tractoken' is set
+        if ($tractoken === null || (is_array($tractoken) && count($tractoken) === 0)) {
+            throw new \InvalidArgumentException(
+                'Missing the required parameter $tractoken when calling devicesNearestGet'
+            );
+        }
+        // verify the required parameter 'latitude' is set
+        if ($latitude === null || (is_array($latitude) && count($latitude) === 0)) {
+            throw new \InvalidArgumentException(
+                'Missing the required parameter $latitude when calling devicesNearestGet'
+            );
+        }
+        // verify the required parameter 'longitude' is set
+        if ($longitude === null || (is_array($longitude) && count($longitude) === 0)) {
+            throw new \InvalidArgumentException(
+                'Missing the required parameter $longitude when calling devicesNearestGet'
+            );
+        }
+
+        $resourcePath = '/devices/nearest';
+        $formParams = [];
+        $queryParams = [];
+        $headerParams = [];
+        $httpBody = '';
+        $multipart = false;
+
+        // query params
+        if ($tractoken !== null) {
+            $queryParams['tractoken'] = ObjectSerializer::toQueryValue($tractoken);
+        }
+        // query params
+        if ($latitude !== null) {
+            $queryParams['latitude'] = ObjectSerializer::toQueryValue($latitude);
+        }
+        // query params
+        if ($longitude !== null) {
+            $queryParams['longitude'] = ObjectSerializer::toQueryValue($longitude);
+        }
+
+
+        // body params
+        $_tempBody = null;
+
+        if ($multipart) {
+            $headers = $this->headerSelector->selectHeadersForMultipart(
+                ['application/json']
+            );
+        } else {
+            $headers = $this->headerSelector->selectHeaders(
+                ['application/json'],
+                ['application/json']
+            );
+        }
+
+        // for model (json/xml)
+        if (isset($_tempBody)) {
+            // $_tempBody is the method argument, if present
+            $httpBody = $_tempBody;
+            // \stdClass has no __toString(), so we should encode it manually
+            if ($httpBody instanceof \stdClass && $headers['Content-Type'] === 'application/json') {
+                $httpBody = \GuzzleHttp\json_encode($httpBody);
+            }
+        } elseif (count($formParams) > 0) {
+            if ($multipart) {
+                $multipartContents = [];
+                foreach ($formParams as $formParamName => $formParamValue) {
+                    $multipartContents[] = [
+                        'name' => $formParamName,
+                        'contents' => $formParamValue
+                    ];
+                }
+                // for HTTP post (form)
+                $httpBody = new MultipartStream($multipartContents);
+
+            } elseif ($headers['Content-Type'] === 'application/json') {
+                $httpBody = \GuzzleHttp\json_encode($formParams);
+
+            } else {
+                // for HTTP post (form)
+                $httpBody = \GuzzleHttp\Psr7\build_query($formParams);
+            }
+        }
+
+        // this endpoint requires HTTP basic authentication
+        if ($this->config->getUsername() !== null || $this->config->getPassword() !== null) {
+            $headers['Authorization'] = 'Basic ' . base64_encode($this->config->getUsername() . ":" . $this->config->getPassword());
+        }
+
+        $defaultHeaders = [];
+        if ($this->config->getUserAgent()) {
+            $defaultHeaders['User-Agent'] = $this->config->getUserAgent();
+        }
+
+        $headers = array_merge(
+            $defaultHeaders,
+            $headerParams,
+            $headers
+        );
+
+        $query = \GuzzleHttp\Psr7\build_query($queryParams);
+        return new Request(
+            'GET',
+            $this->config->getHost() . $resourcePath . ($query ? "?{$query}" : ''),
+            $headers,
+            $httpBody
+        );
+    }
+
+    /**
      * Operation devicesPost
      *
      * Create a Device
@@ -14736,14 +15028,15 @@ class DefaultApi
      *
      * @param  string $token token (optional)
      * @param  int $geo_id geo_id (optional)
+     * @param  string $app_version app_version (optional)
      *
      * @throws \Swagger\Client\ApiException on non-2xx response
      * @throws \InvalidArgumentException
      * @return \Swagger\Client\Model\User
      */
-    public function sessionRegisterGet($token = null, $geo_id = null)
+    public function sessionRegisterGet($token = null, $geo_id = null, $app_version = null)
     {
-        list($response) = $this->sessionRegisterGetWithHttpInfo($token, $geo_id);
+        list($response) = $this->sessionRegisterGetWithHttpInfo($token, $geo_id, $app_version);
         return $response;
     }
 
@@ -14754,15 +15047,16 @@ class DefaultApi
      *
      * @param  string $token (optional)
      * @param  int $geo_id (optional)
+     * @param  string $app_version (optional)
      *
      * @throws \Swagger\Client\ApiException on non-2xx response
      * @throws \InvalidArgumentException
      * @return array of \Swagger\Client\Model\User, HTTP status code, HTTP response headers (array of strings)
      */
-    public function sessionRegisterGetWithHttpInfo($token = null, $geo_id = null)
+    public function sessionRegisterGetWithHttpInfo($token = null, $geo_id = null, $app_version = null)
     {
         $returnType = '\Swagger\Client\Model\User';
-        $request = $this->sessionRegisterGetRequest($token, $geo_id);
+        $request = $this->sessionRegisterGetRequest($token, $geo_id, $app_version);
 
         try {
             $options = $this->createHttpClientOption();
@@ -14830,13 +15124,14 @@ class DefaultApi
      *
      * @param  string $token (optional)
      * @param  int $geo_id (optional)
+     * @param  string $app_version (optional)
      *
      * @throws \InvalidArgumentException
      * @return \GuzzleHttp\Promise\PromiseInterface
      */
-    public function sessionRegisterGetAsync($token = null, $geo_id = null)
+    public function sessionRegisterGetAsync($token = null, $geo_id = null, $app_version = null)
     {
-        return $this->sessionRegisterGetAsyncWithHttpInfo($token, $geo_id)
+        return $this->sessionRegisterGetAsyncWithHttpInfo($token, $geo_id, $app_version)
             ->then(
                 function ($response) {
                     return $response[0];
@@ -14851,14 +15146,15 @@ class DefaultApi
      *
      * @param  string $token (optional)
      * @param  int $geo_id (optional)
+     * @param  string $app_version (optional)
      *
      * @throws \InvalidArgumentException
      * @return \GuzzleHttp\Promise\PromiseInterface
      */
-    public function sessionRegisterGetAsyncWithHttpInfo($token = null, $geo_id = null)
+    public function sessionRegisterGetAsyncWithHttpInfo($token = null, $geo_id = null, $app_version = null)
     {
         $returnType = '\Swagger\Client\Model\User';
-        $request = $this->sessionRegisterGetRequest($token, $geo_id);
+        $request = $this->sessionRegisterGetRequest($token, $geo_id, $app_version);
 
         return $this->client
             ->sendAsync($request, $this->createHttpClientOption())
@@ -14902,11 +15198,12 @@ class DefaultApi
      *
      * @param  string $token (optional)
      * @param  int $geo_id (optional)
+     * @param  string $app_version (optional)
      *
      * @throws \InvalidArgumentException
      * @return \GuzzleHttp\Psr7\Request
      */
-    protected function sessionRegisterGetRequest($token = null, $geo_id = null)
+    protected function sessionRegisterGetRequest($token = null, $geo_id = null, $app_version = null)
     {
 
         $resourcePath = '/session/register';
@@ -14923,6 +15220,10 @@ class DefaultApi
         // query params
         if ($geo_id !== null) {
             $queryParams['geoId'] = ObjectSerializer::toQueryValue($geo_id);
+        }
+        // query params
+        if ($app_version !== null) {
+            $queryParams['appVersion'] = ObjectSerializer::toQueryValue($app_version);
         }
 
 
